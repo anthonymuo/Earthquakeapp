f883a77f43c65431a37321a1012598e5ff42c873
anthonymuo <a.c.muo.16@aberdeen.ac.uk>
2017-12-21 01:00:00 +0000

    Navigation, welcome controller and root configured

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
-Rails.application.routes.draw do
+ Rails.application.routes.draw do
+  get 'welcome/index'
+
+  get 'welcome/about'
+
+  get 'welcome/contact'
+
+  get 'welcome/features'
+
+  get 'welcome/faq'
+
+  get 'welcome/data'
+
   resources :earthquakes
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root to: 'welcome#index' 
+  
+  
 end
