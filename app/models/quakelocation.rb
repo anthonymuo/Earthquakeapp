3735bfe04f5d199451a5178d7915faf8586a10f9
anthonymuo <a.c.muo.16@aberdeen.ac.uk>
2017-12-31 18:36:33 +0000

    deployment fixies

--- app/models/quakelocation.rb
+++ app/models/quakelocation.rb
@@ -1,6 +1,6 @@
 class Quakelocation < ApplicationRecord
 #attr_accessor :adgress, :latitude, :longitude
-  geocoded_by :full_street_address
+  geocoded_by :address
   after_validation :geocode, :if => :address_changed?
   
   
