031d140ca0e99b73e8535cd090d89aae96da09f1
anthonymuo <a.c.muo.16@aberdeen.ac.uk>
2017-12-31 14:47:09 +0000

    fixing deloyingment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,7 @@ config.action_mailer.delivery_method = :smtp
 config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = false
 config.action_mailer.default :charset => "utf-8"
-config.action_mailer.default_url_options = { :host => 'earthquakeap.herokuapp.com' }
+config.action_mailer.default_url_options = { :host => 'https://earthquakeap.herokuapp.com' }
 
 config.action_mailer.smtp_settings = {
   :user_name => ENV[app83639240@heroku.com],
