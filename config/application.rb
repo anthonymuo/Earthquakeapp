18dc47bdacdd006cb8f8ccd96bc778a7a3164cf1
anthonymuo <a.c.muo.16@aberdeen.ac.uk>
2017-12-31 14:30:53 +0000

    deploy fixies

--- config/application.rb
+++ config/application.rb
@@ -15,20 +15,7 @@ module EarthMonitorApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-<<<<<<< HEAD
-=======
- 
- 
- ActionMailer::Base.smtp_settings = {
-  :user_name => 'app83639240@heroku.com',
-  :password => 'pl8cmddu4697',
-  :domain => 'earthquakeap.herokuapp.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
->>>>>>> sendgrid
+
 
   end
 end
